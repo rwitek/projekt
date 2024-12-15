4e902add28ced1a773d218c471fa4b5a8ff0c18e
rafal.witek <116377662+rwitek@users.noreply.github.com>
2024-12-15 20:54:05 +0000

    Adding products to main.py

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def hello_name(name):
     return 'Hello %s!' % name
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/products')
+def products():
+    product_list = ["Apple", "Banana", "Orange"]
+    html = "<h1>Lista produktów</h1><ul>"
+    for p in product_list:
+        html += f"<li>{p}</li>"
+    html += "</ul>"
+    return html
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
